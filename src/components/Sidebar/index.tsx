fd405cad944c1acd4f0b0963d9e254b85eb9cda9
Thiago <thiago.mattos@ejcm.com.br>
2022-03-05 19:00:56 -0300

    feat: started sidebarMenu

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity } from "react-native";
+
+import { SidebarBackground, SidebarSandwichIcon, SidebarNode, SidebarNodeText } from './styles'
+
+import FeatherIcons from 'react-native-vector-icons/Feather'
+
+const Sidebar = () => {
+    const [isOpen, setIsOpen] = useState(true);
+
+    return(
+        <SidebarBackground isOpen={isOpen}>
+            <SidebarSandwichIcon isOpen={isOpen} onPress={() =>setIsOpen(!isOpen)}>
+                <FeatherIcons name='menu' size={25}/>
+            </SidebarSandwichIcon>
+
+            <SidebarNode isOpen={isOpen}>
+                <FeatherIcons name='home' size={25} />
+                <SidebarNodeText isOpen={isOpen}>Dashboard</SidebarNodeText>
+            </SidebarNode>
+            <SidebarNode isOpen={isOpen}>
+                <FeatherIcons name='pie-chart' size={25} />
+                <SidebarNodeText isOpen={isOpen}>Gráficos</SidebarNodeText>
+            </SidebarNode>
+            <SidebarNode isOpen={isOpen}>
+                <FeatherIcons name='user' size={25} />
+                <SidebarNodeText isOpen={isOpen}>Usuários</SidebarNodeText>
+            </SidebarNode>
+        </SidebarBackground>
+    )
+}
+
+export default Sidebar;
---
+++ src/components/Sidebar/styles.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components/native';
+
+export const SidebarBackground = styled.View`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: blue;
+    height: 100%;
+    width: ${props => props.isOpen ? "250px" : "80px"};
+    padding: ${props => props.isOpen ? "15px" : "15px 0"};
+    transition: width 0.2s linear;
+`;
+
+export const SidebarSandwichIcon = styled.TouchableOpacity`
+    position: ${props => props.isOpen ? "realtive" : "inherit"};
+    margin-right: ${props => props.isOpen ? "10px" : "0"};
+    margin-left: ${props => props.isOpen ? "auto" : "0"};
+    margin-bottom: 10px;
+`;
+
+export const SidebarNode = styled.TouchableOpacity`
+    margin-top: 10px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: ${props => props.isOpen ? "" : "center"};
+    padding: 10px;
+    width: ${props => props.isOpen ? "90%" : "80%"};
+    background-color: white;
+    border-radius: 10px;
+    transition: none;
+`;
+
+export const SidebarNodeText = styled.Text`
+    display: ${props => props.isOpen ? "inline" : "none"};
+    visibility: ${props => props.isOpen ? "visible" : "hidden"};
+    margin-left: 10px;
+`;
