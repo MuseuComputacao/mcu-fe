5c6baf4bac0116635a64bef04b605340171347ae
Thiago Barcellos Mattos <th.mattos04@gmail.com>
2022-03-23 16:24:22 -0300

    feat(globalStyles.ts): created globalStyles script

---
+++ src/globalStyles.ts
@@ -0,0 +1,11 @@
+export const style = {
+    colors: {
+      primary: "#AD4A4A",
+      lighterPrimary: "rgba(173, 74, 74 ,0.5)",
+      backgroundColor: "#18121E",
+      terciary: "#f7e0bc",
+      black: "#000000",
+      white: "#FFFFFF",
+    },
+    borderRadius: "10px",
+}
