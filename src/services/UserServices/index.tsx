75765e34de74a3f241ec13dca70d3a2c32250f14
Thiago Barcellos Mattos <th.mattos04@gmail.com>
2022-05-04 16:46:25 -0300

    refactor(api requests): changed api requests to centralize baseUrl

---
+++ src/services/UserServices/index.tsx
@@ -0,0 +1,27 @@
+import api from '../api';
+
+interface FormData {
+    email: string;
+    password: string;
+}
+
+interface Config {
+    headers: {
+      'access-token': string,
+      uid: string,
+      client: string,
+    }
+  }
+
+
+export default {
+    async login(Data: FormData) {
+        const response = await api.post('api/auth/sign_in', Data);
+        return response;
+    },
+
+    async logout(Data: Config) {
+        const response = await api.delete('api/auth/sign_out', Data);
+        return response;
+    }
+}
