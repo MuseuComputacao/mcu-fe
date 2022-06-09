8e5ae75f559cfd08f0f24dd5002fb4b8b3f8d549
Fábio R. Nóbrega <fabio.r.nobrega@gmail.com>
2022-06-09 19:32:33 -0300

    feat(yarn.lock): use yarn insted npm

--- src/components/Sidebar/styles.tsx
+++ src/components/Sidebar/styles.tsx
@@ -9,11 +9,10 @@ export const SidebarBackground = styled.View`
     height: 100%;
     width: ${props => props.isOpen ? "250px" : "80px"};
     padding: ${props => props.isOpen ? "15px" : "15px 0"};
-    transition: width 0.2s linear;
 `;
 
 export const SidebarSandwichIcon = styled.TouchableOpacity`
-    position: ${props => props.isOpen ? "realtive" : "inherit"};
+    position: ${props => props.isOpen ? "relative" : "inherit"};
     margin-right: ${props => props.isOpen ? "10px" : "0"};
     margin-left: ${props => props.isOpen ? "auto" : "0"};
     margin-bottom: 10px;
@@ -24,12 +23,11 @@ export const SidebarNode = styled.TouchableOpacity`
     display: flex;
     flex-direction: row;
     align-items: center;
-    justify-content: ${props => props.isOpen ? "" : "center"};
+    justify-content: ${props => props.isOpen ? "flex-start" : "center"};
     padding: 10px;
     width: ${props => props.isOpen ? "90%" : "80%"};
     background-color: ${style.colors.white};
     border-radius: 10px;
-    transition: none;
 
     &:hover{
         background-color: ${style.colors.primary};
@@ -37,7 +35,6 @@ export const SidebarNode = styled.TouchableOpacity`
 `;
 
 export const SidebarNodeText = styled.Text`
-    display: ${props => props.isOpen ? "inline" : "none"};
-    visibility: ${props => props.isOpen ? "visible" : "hidden"};
+    display: ${props => props.isOpen ? "flex" : "none"};
     margin-left: 10px;
 `;
