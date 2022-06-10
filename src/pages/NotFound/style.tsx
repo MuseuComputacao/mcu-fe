99cca5e1aedfb25c5b328c37adc5a5d826126573
Fábio R. Nóbrega <fabio.r.nobrega@gmail.com>
2022-06-10 09:09:36 -0300

    feat(src/pages/NotFound/): add notfound page

---
+++ src/pages/NotFound/style.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+
+export const NotFoundView = styled.View`
+  margin: auto;
+  text-align: center;
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,10 @@ import SignIn from './SignIn';
 import SignUp from './SignUp';
 import Dashboard from  './Dashboard'
 import Users from './Users'
+import NotFound  from './NotFound';
 
  export {
+  NotFound,
   Home,
   SignIn,
   SignUp,
