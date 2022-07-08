b3f386be1f81324266ddccf04b0ff46781016834
Fábio R. Nóbrega <fabio.r.nobrega@gmail.com>
2022-07-08 09:44:20 -0300

    feat(src/pages/Items/index.tsx): handler sideBar and container

--- src/pages/Items/index.tsx
+++ src/pages/Items/index.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+
+import { DashboardView } from "../Dashboard/styles";
+
+import Sidebar from "../../components/Sidebar";
+import { DataTable } from 'react-native-paper';
+import { View, Text } from 'react-native';
+
+
 
 const Items = (props: any) => {
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+
+  function getIsOpenProp(getIsOpen: boolean) {
+    setIsOpen(getIsOpen);
+  }
+
+  
     return (
-      <View style={{ width: '100%', height: '100vh'}}>
-        <h3> List of Items </h3>
+      <View style={{ display: 'flex', flexDirection: 'row', height: '100%' }}>
+        <Sidebar func={getIsOpenProp} />
+
+        <DashboardView isOpen={isOpen}>
+          <View style={{ padding: 30 }}>
+            <Text style={{ fontSize: 23 }}>
+                Lista de Items:
+            </Text>
+            <DataTable>
+              
+            </DataTable>
+          </View>
+        </DashboardView>
       </View>
     );
   }
